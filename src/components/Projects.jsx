7f2008fae8513cf18085e5960b6bc3b0df06fdbd
moods-1 <ceemoods@gmail.com>
2024-10-07 15:23:26 -0400

    Update subtext in the projects component.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,9 +21,9 @@ const Works = () => {
 				<SectionHeader subText='My work' headText='Projects.' />
 				<div className='w-full flex'>
 					<p className='mt-3 text-gray-300 text-[17px] max-w-3xl leading-[30px]'>
-						The following projects showcases my skills and experience through
-						real-world examples of my work. Each project is briefly described
-						with, links to code repositories and live demos. It reflects my
+						The following projects showcase my skills and experience through
+						real-world examples of my work. Each project is briefly described, and 
+						has links to code repositories and live demos. They reflect my
 						abilities to solve complex problems, work with different
 						technologies, and manage projects effectively.
 					</p>
